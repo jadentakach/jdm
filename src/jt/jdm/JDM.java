943963d6c5aa991c140925650598a0a02d1d0368
jadentakach <jadentakach@gmail.com>
2026-08-28 00:58:39 -0400

    jdm app init

---
+++ src/jt/jdm/JDM.java
@@ -0,0 +1,22 @@
+package jt.jdm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class JDM {
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jdm [script]");
+            System.exit(24);
+        } else if (args.length == 1) {
+            //
+        } else {
+            //
+        }
+    }
+}
