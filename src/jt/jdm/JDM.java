3a167fe5e366e775566c799b77f97d04295cbb26
jadentakach <jadentakach@gmail.com>
2026-08-28 01:09:45 -0400

    implement run

--- src/jt/jdm/JDM.java
+++ src/jt/jdm/JDM.java
@@ -22,6 +22,22 @@ public class JDM {
 
     public static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        while (true) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+        }
+    }
+
+    private static void run(String source) {
         //
     }
 }
