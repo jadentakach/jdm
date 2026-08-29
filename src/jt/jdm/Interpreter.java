f0c7d6e22ef290e8bd7b4ba016912348773465eb
jadentakach <jadentakach@gmail.com>
2026-08-28 21:30:22 -0400

    variable declaration

--- src/jt/jdm/Interpreter.java
+++ src/jt/jdm/Interpreter.java
@@ -1,12 +1,13 @@
 package jt.jdm;
 
+import java.util.List;
+
 import jt.jdm.TokenType.*;
 
-class Interpreter implements Expr.Visitor<Object> {
-    void interpret(Expr expression) {
+class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+    void interpret(List<Stmt> expression) {
         try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(expression));
+            for (Stmt statement : statements)
         } catch (RuntimeError error) {
             JDM.runtimeError(error);
         }
@@ -77,6 +78,23 @@ class Interpreter implements Expr.Visitor<Object> {
 
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
     }
 
     @Override
