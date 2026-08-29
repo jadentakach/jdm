4f1dc8604b4ad8c6920af0839248223ae567211e
jadentakach <jadentakach@gmail.com>
2026-08-28 20:49:07 -0400

    define type impl visitors

--- src/jt/tool/GenerateAst.java
+++ src/jt/tool/GenerateAst.java
@@ -39,6 +39,9 @@ public class GenerateAst {
             defineType(writer, baseName, className, fields);
         }
 
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.close();
     }
@@ -71,6 +74,12 @@ public class GenerateAst {
             writer.println("    final " + field + ";");
         }
 
+        writer.println("    }");
+
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("    return visitor.visit" + className + baseName + "(this);");
         writer.println("    }");
     }
 
