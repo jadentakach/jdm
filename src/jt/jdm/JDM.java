bef070e6f970ebe41c869921934b1a66f386ba05
jadentakach <jadentakach@gmail.com>
2026-08-28 22:43:45 -0400

    impl resolver @ JDM

--- src/jt/jdm/JDM.java
+++ src/jt/jdm/JDM.java
@@ -52,6 +52,9 @@ public class JDM {
 
         if (hadError) return;
 
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+
         interpreter.interpret(statements);
     }
 
