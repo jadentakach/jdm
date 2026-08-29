cf6c67021c6f280b046e800ed98bc55a2a9c9759
jadentakach <jadentakach@gmail.com>
2026-08-28 20:47:26 -0400

    define visitor

--- src/jt/tool/GenerateAst.java
+++ src/jt/tool/GenerateAst.java
@@ -31,6 +31,8 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
@@ -39,6 +41,17 @@ public class GenerateAst {
 
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("    }");
     }
 
     private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
@@ -61,5 +74,5 @@ public class GenerateAst {
         writer.println("    }");
     }
 
-    // https://craftinginterpreters.com/representing-code.html#visitors-for-expressions
+    
 }
